2bf806077fb6849b79daca27df3b402724c883b7
jeremyduchesne <duchesnejeremy@gmail.com>
2017-03-30 10:37:11 +0200

    MainActor

--- src/main/scala/be/info/unamur/actors/CityActor.scala
+++ src/main/scala/be/info/unamur/actors/CityActor.scala
@@ -1,11 +1,14 @@
 package be.info.unamur.actors
 
 import akka.actor.Actor
+import com.phidgets.{InterfaceKitPhidget, RFIDPhidget}
 
 /**
   * @author jeremyduchesne
   */
 class CityActor extends Actor {
+
+  val ik = new InterfaceKitPhidget()
 
   sealed trait Message
 
@@ -13,8 +16,16 @@ class CityActor extends Actor {
   case class SwitchOn() extends Message
   case class SwitchOff() extends Message
   case class Blink() extends Message
+  case class Close() extends Message
 
   override def receive: Receive = {
-    case Init(_) =>
+    case Init => {
+      ik openAny()
+
+      ik waitForAttachment()
+    }
+    case Close => {
+      ik clone()
+    }
   }
 }
