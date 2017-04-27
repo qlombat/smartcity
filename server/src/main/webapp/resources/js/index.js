44dbc47fe4a868e4633f9d3e53c65206f001d5b6
Quentin Lombat <lombatquentin@live.fr>
2017-04-27 13:28:11 +0200

    Reload data of sensors in JavaScript

--- server/src/main/webapp/resources/js/index.js
+++ server/src/main/webapp/resources/js/index.js
@@ -8,21 +8,21 @@ $(document).ready(function () {
     function updateSensors(){
         console.log("coucou");
         $.getJSON("http://localhost:8080/api/sensors/temperature", function (data) {
-            if(data.value == null){
+            if(typeof data.value === 'undefined'){
                 $("#temp-value").text("Unavailable");
             }else{
                 $("#temp-value").text(data.value + "° C");
             }
         });
         $.getJSON("http://localhost:8080/api/sensors/light", function (data) {
-            if(data.value == null){
+            if(typeof data.value === 'undefined'){
                 $("#luminosity").text("Unavailable");
             }else{
                 $("#luminosity").text(data.value + " lux");
             }
         });
         $.getJSON("http://localhost:8080/api/sensors/humidity", function (data) {
-            if(data.value == null){
+            if(typeof data.value === 'undefined'){
                 $("#humidity").text("Unavailable");
             }else{
                 $("#humidity").text(data.value + " %");
