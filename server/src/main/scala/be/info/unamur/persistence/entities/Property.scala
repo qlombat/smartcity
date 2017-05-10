acb550facc1120da788036250c7c2a7995aa60f9
jeremyduchesne <duchesnejeremy@gmail.com>
2017-05-10 14:51:50 +0200

    Update of the speed with Temperature, Humidity and Light

--- server/src/main/scala/be/info/unamur/persistence/entities/Property.scala
+++ server/src/main/scala/be/info/unamur/persistence/entities/Property.scala
@@ -6,35 +6,35 @@ import scalikejdbc._
   *
   * @author jeremyduchesne
   */
-case class Property(key:String,value:Double)
+case class Property(id:String,value:Double)
 
 object Property extends SQLSyntaxSupport[Property] {
   override val tableName   = "properties"
-  override val columns     = Seq("key","value")
+  override val columns     = Seq("id","value")
   override val autoSession = AutoSession
   val property: QuerySQLSyntaxProvider[scalikejdbc.SQLSyntaxSupport[Property], Property] = Property.syntax("p")
 
   def apply(p: ResultName[Property])(ps: WrappedResultSet): Property = new Property(
-    key = ps.string(p.key),
+    id = ps.string(p.id),
     value = ps.double(p.value)
   )
 
-  def find(key: String)(implicit session: DBSession = autoSession): Option[Property] = {
+  def find(id: String)(implicit session: DBSession = autoSession): Option[Property] = {
     withSQL {
-      select.from(Property as property).where.eq(property.key, key)
+      select.from(Property as property).where.eq(property.id, id)
     }.map(Property(property.resultName)).first.apply()
   }
 
-  def create(key:String,value:Double)(implicit session: DBSession = autoSession):Property = {
+  def create(id:String,value:Double)(implicit session: DBSession = autoSession):Property = {
     Property(
-      key = key,
+      id = id,
       value = value)
   }
 
   def save(p: Property)(implicit session: DBSession = autoSession): Property = {
     withSQL {
       update(Property as property).set(
-        property.key -> p.key,
+        property.id -> p.id,
         property.value -> p.value
       )
     }.update().apply()
@@ -43,7 +43,7 @@ object Property extends SQLSyntaxSupport[Property] {
 
   def destroy(p: Property)(implicit session: DBSession = autoSession): Unit = {
     withSQL {
-      delete.from(Property).where.eq(column.key, p.value)
+      delete.from(Property).where.eq(column.id, p.value)
     }.update.apply()
   }
 }
